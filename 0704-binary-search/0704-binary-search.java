3c8599efde1e099d8a0b8e9a929bb8bf002e0999
Ahmed Ashraf <41305524+ahmedashrfhassan@users.noreply.github.com>
2024-01-25 15:39:19 +0300

    Time: 0 ms (100.00%), Space: 45.7 MB (27.57%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int result = Arrays.binarySearch(nums, target);
+        if (result>=0) return result;
+        else return -1;
+    }
+}
