1668ba8ecff92c774b001096eac331fe0bfdef13
Ahmed Ashraf <41305524+ahmedashrfhassan@users.noreply.github.com>
2025-06-30 10:18:13 +0300

    Time: 10 ms (32.43%), Space: 44.8 MB (34.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-            if(s.length() != t.length()){
-                return false;
-            }else {
-                Map<Character, Integer> charToOccurrence = new HashMap<>();
-                for (int i =0 ; i< s.length() ; i++) {
-                    char currentChar = s.charAt(i);
-                    if (charToOccurrence.containsKey(currentChar)) {
-                        charToOccurrence.replace(currentChar, (charToOccurrence.get(currentChar) + 1));
-                    } else {
-                        charToOccurrence.put(currentChar, 1);
-                    }
-                }
-                for (int i =0 ; i< t.length() ; i++) {
-                    char currentChar = t.charAt(i);
-                    if (charToOccurrence.containsKey(currentChar)) {
-                        Integer occurenceOfCurrentChar = charToOccurrence.get(currentChar);
-                        if (occurenceOfCurrentChar == 0) return false;
-                        charToOccurrence.replace(currentChar, (occurenceOfCurrentChar - 1));
-                    } else {
-                        return false;
-                    }
-                }
-                boolean empty = !charToOccurrence.values().stream().anyMatch(v -> v >0);
-                return empty;
-            }
+        Map<Character,Integer> map = new HashMap<>();
+        for (Character c : s.toCharArray()){ 
+            icreaseCharOccurrence(map, c);
+        }
+        for (Character c : t.toCharArray()){
+            var value = map.get(c);
+            if (value == null) return false;
+            else if (value == 1) map.remove(c);
+            else map.put(c, value-1);
+        }
+        return map.isEmpty();
+    }
+    
+    private void icreaseCharOccurrence(Map<Character, Integer> map, Character c){
+        map.compute(c, (k,v)-> v==null? 1 : v + 1);
     }
 }
