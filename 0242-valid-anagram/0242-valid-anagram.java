159e434d9cb2656a66bb9df23296f5baa0d304c4
Ahmed Ashraf <41305524+ahmedashrfhassan@users.noreply.github.com>
2025-06-30 10:19:57 +0300

    Time: 4 ms (77.63%), Space: 44.6 MB (42.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map = new HashMap<>();
-        for (Character c : s.toCharArray()){ 
-            icreaseCharOccurrence(map, c);
-        }
-        for (Character c : t.toCharArray()){
-            var value = map.get(c);
-            if (value == null) return false;
-            else if (value == 1) map.remove(c);
-            else map.put(c, value-1);
-        }
-        return map.isEmpty();
-    }
-    
-    private void icreaseCharOccurrence(Map<Character, Integer> map, Character c){
-        map.compute(c, (k,v)-> v==null? 1 : v + 1);
-    }
-}
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch1,ch2);
+    }}
